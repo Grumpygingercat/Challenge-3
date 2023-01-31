bfc803c08dfa8c9d232b0780572f5fa44338c7e1
Melvin Zacharias <zacharias.melvin@gmail.com>
2023-01-31 10:05:31 +0100

    typo

--- challenge3createtablesandstructure.sql
+++ challenge3createtablesandstructure.sql
@@ -12,19 +12,19 @@ CREATE TABLE `contactformular` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `customer` (
-  'user_id' int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  'phone' varchar(16) NOT NULL,
-  'address' varchar(200) NOT NULL
+  `phone` varchar(16) NOT NULL,
+  `address` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE 'orders' (
-  'order_id' int(11) NOT NULL AUTO_INCREMENT,
-  'user_id' int(11),
-  'ordertime' timestamp NOT NULL DEFAULT current_timestamp(),
-  'ordersize' varchar(4)
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11),
+  `ordertime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ordersize` varchar(4)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE `contactformular`
@@ -35,7 +35,7 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`user_id`);
 COMMIT;
 
-ALTER TABLE 'orders'
-  ADD PRIMARY KEY ('order_id'),
-  ADD FOREIGN KEY ('user_id') REFERENCES customer('user_id');
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`oder_id`),
+  ADD FOREIGN KEY (`user_id`) REFERENCES customer(`user_id`);
 
