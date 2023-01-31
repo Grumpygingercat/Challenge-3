fc6dacf2dab476cd14510b91dfc21509a7a93842
Melvin Zacharias <zacharias.melvin@gmail.com>
2023-01-31 11:18:51 +0100

    changed sql

---
+++ challenge3createtablesandstructure.sql
@@ -0,0 +1,32 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `contactformular` (
+  `contact_id` timestamp PRIMARY KEY NOT NULL DEFAULT current_timestamp(),
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `customer` (
+  `user_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(16) NOT NULL,
+  `address` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `orders` (
+  `order_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int(11),
+  `ordertime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ordersize` varchar(4)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `orders`
+  ADD FOREIGN KEY (`user_id`) REFERENCES customer(`user_id`);
+
