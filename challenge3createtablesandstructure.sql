f878b5e9fe1dc1032c27e76263c2d537cf70540e
Melvin Zacharias <zacharias.melvin@gmail.com>
2023-01-31 12:00:41 +0100

    deleete ``

--- challenge3createtablesandstructure.sql
+++ challenge3createtablesandstructure.sql
@@ -4,29 +4,29 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `contactformular` (
-  `contact_id` timestamp PRIMARY KEY NOT NULL DEFAULT current_timestamp(),
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `message` varchar(2000) NOT NULL
+  contact_id timestamp PRIMARY KEY NOT NULL DEFAULT current_timestamp(),
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  message varchar(2000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `customer` (
-  `user_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  'username' varchar(16) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` varchar(16) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  'password' varchar(50) NOT NULL
+  user_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  username varchar(16) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  phone varchar(16) NOT NULL,
+  address varchar(200) NOT NULL,
+  password varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `orders` (
-  `order_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int(11),
-  `ordertime` timestamp NOT NULL DEFAULT current_timestamp(),
-  `ordersize` varchar(4)
+  order_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  user_id int(11),
+  ordertime timestamp NOT NULL DEFAULT current_timestamp(),
+  ordersize varchar(4)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE `orders`
