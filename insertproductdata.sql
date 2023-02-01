726f8bb63a8f693696ed5dff123758635d59e97d
Melvin Zacharias <zacharias.melvin@gmail.com>
2023-02-01 09:54:54 +0100

    added  insertproductdata sql  TODO longblobs

---
+++ insertproductdata.sql
@@ -0,0 +1,61 @@
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'BASIC', TODO , 's', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'BASIC', TODO , 'm', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'BASIC', TODO , 'l', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'BASIC', TODO , 'xl', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'CREATIVE', TODO , 's', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'CREATIVE', TODO , 'm', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'CREATIVE', TODO , 'l', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'CREATIVE', TODO , 'xl', 19.99
+);
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'SIMPLE', TODO , 's', 19.99
+)
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'SIMPLE', TODO , 'm', 19.99
+)
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'SIMPLE', TODO , 'l', 19.99
+)
+INSERT INTO t_shirt (
+    title, pic, size, price
+) VALUES (
+    'SIMPLE', TODO , 'xl', 19.99
+)
+
