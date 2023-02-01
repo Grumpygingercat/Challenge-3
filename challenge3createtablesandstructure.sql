9ed98fecb5304b1266609286c086636e7af39ec1
Melvin Zacharias <zacharias.melvin@gmail.com>
2023-02-01 09:10:23 +0100

    changed sqlll

--- challenge3createtablesandstructure.sql
+++ challenge3createtablesandstructure.sql
@@ -9,7 +9,7 @@ CREATE TABLE `contactformular` (
   last_name varchar(50) NOT NULL,
   email varchar(50) NOT NULL,
   message varchar(2000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 CREATE TABLE `customer` (
   user_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -20,15 +20,36 @@ CREATE TABLE `customer` (
   phone varchar(16) NOT NULL,
   address varchar(200) NOT NULL,
   password varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 CREATE TABLE `orders` (
   order_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id int(11),
   ordertime timestamp NOT NULL DEFAULT current_timestamp(),
-  ordersize varchar(4)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  ordersize varchar(4),
+  t_shirt_id varchar(5),
+  size enum ('s','m','l','xl') NOT NULL
+);
+
+CREATE TABLE cart (
+  user_id int(11),
+  t_shirt_id int(5),
+  PRIMARY KEY (user_id, t_shirt_id)
+);
+
+CREAT TABLE t_shirt (
+ t_shirt_id int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+ title varchar(100) NOT NULL,
+ pic longblob NOT NULL,
+ size enum('s','m','l','xl') NOT NULL,
+ price double NOT NULL
+)
 
 ALTER TABLE `orders`
   ADD FOREIGN KEY (`user_id`) REFERENCES customer(`user_id`);
+  ADD FOREIGN KEY (size) REFERENCES t_shirt(size);
 
+ALTER TABLE cart  
+  ADD FOREIGN KEY (user_id) REFERENCES customer(user_id);
+ALTER TABLE cart  
+  ADD FOREIGN KEY (t_shirt_id) REFERENCES t_shirt(t_shirt_id);
