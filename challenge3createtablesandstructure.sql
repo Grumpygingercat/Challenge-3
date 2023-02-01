4d2754d4dce364d7a881ea611493364a539f6521
Melvin Zacharias <zacharias.melvin@gmail.com>
2023-02-01 10:25:03 +0100

    damn those FK

--- challenge3createtablesandstructure.sql
+++ challenge3createtablesandstructure.sql
@@ -9,7 +9,7 @@ CREATE TABLE `contactformular` (
   last_name varchar(50) NOT NULL,
   email varchar(50) NOT NULL,
   message varchar(2000) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 CREATE TABLE `customer` (
   user_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -20,19 +20,19 @@ CREATE TABLE `customer` (
   phone varchar(16) NOT NULL,
   address varchar(200) NOT NULL,
   password varchar(50) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 CREATE TABLE `orders` (
   order_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id int(11),
   ordertime timestamp NOT NULL DEFAULT current_timestamp(),
-  t_shirt_id varchar(5),
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  t_shirt_id int(5)
+);
 
 CREATE TABLE cart (
   user_id int(11),
-  t_shirt_id int(5),
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  t_shirt_id int(5)
+);
 
 CREATE TABLE t_shirt (
  t_shirt_id int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -40,10 +40,12 @@ CREATE TABLE t_shirt (
  pic longblob NOT NULL,
  size enum('s','m','l','xl') NOT NULL,
  price double NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 ALTER TABLE `orders`
   ADD FOREIGN KEY (`user_id`) REFERENCES customer(`user_id`);
+ALTER TABLE orders
+  ADD FOREIGN KEY (t_shirt_id) REFERENCES t_shirt(t_shirt_id);
 
 ALTER TABLE cart  
   ADD FOREIGN KEY (user_id) REFERENCES customer(user_id);
